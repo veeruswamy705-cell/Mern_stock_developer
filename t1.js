eb43e72eef343832785558f2bb36992c57cec2aa
Bhadra <veeruswamy705@gmail.com>
2026-02-03 17:51:34 +0530

    add a js file

---
+++ t1.js
@@ -0,0 +1,1 @@
+let somefunc = () => console.log("hello") 
---
+++ t3.js
@@ -0,0 +1,23 @@
+// // let num1=10
+
+
+// // num1 +=20
+// const pi=3.142
+
+// let radius=3
+
+// let radius_square = radius*radius
+
+// let area_of_the_circle=pi*radius_square
+let num1=10
+let num2=20
+
+let output1 = num1 + num2
+let output2=num1-num2
+let output3=num1*num2
+let output4=num1/num2
+
+console.log('additional operator + ==> output is',output1);
+console.log('sum operator - ==> output is',output2);
+console.log('mul operator * ==> output is',output3);
+console.log('div operator % ==> output is',output4);

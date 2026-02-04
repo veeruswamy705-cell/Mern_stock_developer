d40cf24ebdb690054f4b34b08ce4094e42c36382
Bhadra <veeruswamy705@gmail.com>
2026-02-04 17:54:31 +0530

    add a new js file

---
+++ t6.js
@@ -0,0 +1,42 @@
+// let username = [];
+
+// if(username){
+//     console.log('\n\n\n\n you are logged in');
+// }
+// else{
+//     console.log('\n\n\n\n please login');
+// }
+
+// let num1=10
+
+
+// console.log(++num1);
+// console.log(num1);
+// let a=null;
+
+
+
+// console.log(a);
+
+// let itisraining = true;
+// let itissunny= false;
+
+
+// console.log( !itissunny);
+
+let marks=30
+
+switch(marks) {
+    case 30:
+        console.log("you are failed");
+        break;
+    case 50:
+        console.log("you have passed");
+        break;
+    case 80:
+        console.log("you have got distinction");
+        break;
+    default:
+        console.log("invalid marks");
+
+}
---
+++ t7.js
@@ -0,0 +1,4 @@
+// function sayhello(){
+//     alert('hello user');
+// }
+   

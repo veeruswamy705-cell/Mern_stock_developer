e6f971ca18a46ee94620a08a59efdbd3e6e11e6d
Bhadra <veeruswamy705@gmail.com>
2026-02-05 17:38:53 +0530

    add t8.js file

---
+++ t8.js
@@ -0,0 +1,88 @@
+// let a=10
+// let b=20**2
+
+// console.log(b);
+
+
+// function test(){
+
+//     if(true){
+//         var a=10;
+
+//         console.log(a);
+//     }
+
+// }
+
+
+// test();
+
+
+// let a=10
+
+// let b="10"
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a>=b);
+// console.log(a<=b);
+// console.log(a==b);
+// console.log(a!=b);
+// console.log(a===b);
+
+// let a=75;
+
+
+
+// if(a==30){
+//     console.log("the student was pass");
+// }
+// else if(a>=30 && a<50){
+//     console.log("the student was average");
+// }
+// else if(a>=50 && a<80){
+//     console.log("the student was good");
+// }
+// else{
+//     console.log("the student was fail");
+// }
+
+
+// let username="sanjay"
+// let password="sanjay123"
+
+// if(username=="sanjay"){
+//     console.log("username is correct");
+//     if(password=="sanjay123"){
+//         console.log("password is correct");
+//     }
+//     else{
+//         console.log("password is incorrect");
+//     }
+
+// }
+// else{
+//     console.log("username is incorrect");
+// }
+
+let day=5
+
+
+switch(day){
+    case 1:
+        console.log("the day is monday");
+        break;
+        case 2:
+        console.log("the day is tuesday");
+        break;
+        case 3:
+        console.log("the day is wednesday");
+        break;
+        default:
+        console.log("the day is not found");
+
+
+}
+
+
+
+
